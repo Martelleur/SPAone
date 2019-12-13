c4b3d9a49e1ccbc9686c7e2045b1e76a2223eaaf
Martelleur <joel.martelleur@hotmail.com>
2019-12-13 17:32:54 +0100

    test solution for moving

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,19 @@
 import './chat-app.js'
+import { moveElement } from './moveElement.js'
 
+let chatId
+let counterChats = 0
 document.querySelector('#chatButton').addEventListener('click', (event) => {
+  event.preventDefault()
+  counterChats++
   const chat = document.createElement('chat-app')
+  chatId = `chat${counterChats}`
+  chat.setAttribute('id', chatId)
+  document.querySelector('#chatWrapper').appendChild(document.createElement('br'))
   document.querySelector('#chatWrapper').appendChild(chat)
+  document.querySelector('#chatWrapper').appendChild(document.createElement('br'))
+
+  moveElement(chat)
+  console.log('chat.id/JM')
+  console.log(chat.id)
 })

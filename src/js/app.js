cf643474980f43479907cbbc39d4bf43ce9ffad9
Martelleur <joel.martelleur@hotmail.com>
2019-12-13 19:33:41 +0100

    working on memory

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
+import './memory-app.js'
 import './chat-app.js'
 import { moveElement } from './moveElement.js'
 
+// Creating chat-elements
 let chatId
 let counterChats = 0
 document.querySelector('#chatButton').addEventListener('click', (event) => {
@@ -9,11 +11,22 @@ document.querySelector('#chatButton').addEventListener('click', (event) => {
   const chat = document.createElement('chat-app')
   chatId = `chat${counterChats}`
   chat.setAttribute('id', chatId)
-  document.querySelector('#chatWrapper').appendChild(document.createElement('br'))
-  document.querySelector('#chatWrapper').appendChild(chat)
-  document.querySelector('#chatWrapper').appendChild(document.createElement('br'))
+  document.querySelector('#wrapper').appendChild(document.createElement('br'))
+  document.querySelector('#wrapper').appendChild(chat)
+  document.querySelector('#wrapper').appendChild(document.createElement('br'))
 
   moveElement(chat)
   console.log('chat.id/JM')
   console.log(chat.id)
+})
+
+// Creating memory-elements
+document.querySelector('#memoryButton').addEventListener('click', (event) => {
+  event.preventDefault()
+  const memory = document.createElement('memory-app')
+  document.querySelector('#wrapper').appendChild(document.createElement('br'))
+  document.querySelector('#wrapper').appendChild(memory)
+  document.querySelector('#wrapper').appendChild(document.createElement('br'))
+
+  moveElement(memory)
 })

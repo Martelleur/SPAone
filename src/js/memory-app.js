c65d30afa065a0d2ca749061a77a477d4aa65021
Martelleur <joel.martelleur@hotmail.com>
2020-01-16 08:41:32 +0100

    working on memory

--- src/js/memory-app.js
+++ src/js/memory-app.js
@@ -162,6 +162,10 @@ export class Memory extends window.HTMLElement {
     this._commentBox = this.shadowRoot.querySelector('#commentBox')
     this._keyHighscore = true
     this._keyChat = true
+    this._keyTurnBackPictures = true
+    this._keyOpenPictures = true
+    this._timeOut1 = undefined
+    this._timeOut2 = undefined
   }
 
   /**
@@ -340,6 +344,10 @@ export class Memory extends window.HTMLElement {
     // eventlistner for this._memoryPictures
     this._memoryPictures.addEventListener('click', (event) => {
       event.preventDefault()
+      if (!this._keyOpenPictures) {
+        return
+      }
+      this._keyTurnBackPictures = true
       // event.stopPropagation()
 
       if (!this._isStarting) {
@@ -354,6 +362,9 @@ export class Memory extends window.HTMLElement {
       if (this.countOpenTiles() > 1) {
         return
       }
+
+      clearTimeout(this._timeOut1)
+      clearTimeout(this._timeOut2)
 
       // if user click on a turned picture with the mouse
       if (event.target.tagName === 'IMG') {
@@ -402,19 +413,23 @@ export class Memory extends window.HTMLElement {
           for (let i = 0; i < this.shadowRoot.querySelectorAll('#memoryPictures img').length; i++) {
             if (this.shadowRoot.querySelectorAll('#memoryPictures img')[i].getAttribute('src') === this._tempArray[1].getAttribute('src')) {
               this._quantityOfPaires++
+              this._keyTurnBackPictures = false
+              this._keyOpenPictures = false
               this._paires.innerText = `Paires: ${this._quantityOfPaires / 2}\nTries: ${Math.floor(this._tries / 2)}`
-              window.setTimeout(() => {
+              this._timeOut1 = window.setTimeout(() => {
                 this.shadowRoot.querySelectorAll('#memoryPictures img')[i].style.visibility = 'hidden'
+                this._keyOpenPictures = true
               }, 500)
             }
           }
         }
       }
-      // console.log('this.turnbackPictures()/JM')
-      // console.log(this.turnbackPictures())
-      window.setTimeout(() => {
-        this.turnbackPictures()
-      }, 2000)
+
+      if (this._keyTurnBackPictures) {
+        this._timeOut2 = window.setTimeout(() => {
+          this.turnbackPictures()
+        }, 2000)
+      }
 
       // Congrat the user an shoulde later also show result
       window.setTimeout(() => {
